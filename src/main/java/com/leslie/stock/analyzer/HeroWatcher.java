9d38e775354fad6bcd39044998e9e1931704ffaf
RockBoyLeslie <leslie.li@live.com>
2015-02-05 21:26:44 +0800

    修改抓取策略

--- src/main/java/com/leslie/stock/analyzer/HeroWatcher.java
+++ src/main/java/com/leslie/stock/analyzer/HeroWatcher.java
@@ -6,16 +6,13 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
-
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
-
-import org.apache.commons.collections.CollectionUtils;
 import org.jsoup.Connection;
 import org.jsoup.Connection.Request;
 import org.jsoup.Jsoup;
@@ -29,8 +26,7 @@ public class HeroWatcher {
     private static final int CONNECTION_TIME_OUT = 10 * 1000;
 
     private ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
-    private Semaphore printSignal = new Semaphore(1);
-    private List<JSONObject> currentStatus = Collections.emptyList();
+    private Date lastDate = new Date();
     private TranslucentFrame popupWindow = new TranslucentFrame(2, "notice", "");
 
     public void watch(final String id) {
@@ -82,14 +78,25 @@ public class HeroWatcher {
 
             JSONArray statuses = statusJson.getJSONArray("statuses");
             List<JSONObject> newStatuses = new ArrayList<JSONObject>(statuses.size());
-            for (int j = 0; j < statuses.size(); j++) {
-                JSONObject newStatus = new JSONObject();
+            synchronized (lastDate) {
+                System.out.println(lastDate);
+                Date tempDate = lastDate;
+                for (int j = 0; j < statuses.size(); j++) {
+                    JSONObject newStatus = new JSONObject();
 
-                newStatus.put("description", statuses.getJSONObject(j).get("description"));
-                Calendar calendar = Calendar.getInstance();
-                calendar.setTimeInMillis(Long.parseLong(statuses.getJSONObject(j).getString("created_at")));
-                newStatus.put("created_at", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(calendar.getTime()));
-                newStatuses.add(newStatus);
+                    newStatus.put("description", statuses.getJSONObject(j).get("description"));
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.setTimeInMillis(Long.parseLong(statuses.getJSONObject(j).getString("created_at")));
+                    Date createdAt = calendar.getTime();
+                    if (createdAt.after(lastDate)) {
+                        newStatus.put("created_at", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(createdAt));
+                        newStatuses.add(newStatus);
+                        if (createdAt.after(tempDate)) {
+                            tempDate = createdAt;
+                        }
+                    }
+                }
+                lastDate = tempDate;
             }
             return newStatuses;
         }
@@ -97,34 +104,18 @@ public class HeroWatcher {
     }
 
     private void noticePopup(List<JSONObject> newStatuses) {
-        try {
-            printSignal.acquireUninterruptibly();
-            if (CollectionUtils.isEqualCollection(currentStatus, newStatuses)) {
-                return;
-            }
-
-            for (int h = newStatuses.size() - 1; h >= 0; h--) {
-                JSONObject status = newStatuses.get(h);
-
-                if (!currentStatus.contains(status)) {
-                    String message = String.format("%s - %s", status.get("created_at"), status.getString("description"));
-                    System.out.println(message);
-                    popupWindow.setMessage(message);
-                    popupWindow.popup();
-                    //new MessageWindow().popup(message);
-                }
-            }
-            currentStatus = newStatuses;
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            printSignal.release();
+        for (int h = newStatuses.size() - 1; h >= 0; h--) {
+            JSONObject status = newStatuses.get(h);
+            String message = String.format("%s - %s", status.get("created_at"), status.getString("description"));
+            System.out.println(message);
+            popupWindow.setMessage(message);
+            popupWindow.popup();
         }
     }
 
     public static void main(String[] args) {
-        new HeroWatcher().watch("6785033954");
-        //new HeroWatcher().watch("6254653026");
+        // new HeroWatcher().watch("6785033954");
+        new HeroWatcher().watch("6254653026");
     }
 
 }
